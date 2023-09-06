34973ef6b881ad4810b76e0ef7335a1b91a01acd
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-06 15:36:54 -0300

    Adição de lógica para verificar se há pacotes e lógica de compilação para tal caso

--- RunJava.sh
+++ RunJava.sh
@@ -23,23 +23,32 @@ ls src > tempfile.txt # redireciona os arquivos listados para pasta temporária
 SRC="src/"
 DIR_MAIN=""
 CLASS_MAIN="" 
-JAVA_COMPILE=""
-JAVA_EXECUTE="java -cp bin "
+JAVA_COMPILE="javac -d bin"
+JAVA_EXECUTE="java -cp bin"
 COM_PACOTES=0
 SEM_PACOTES=0
 
 # loop para ler cada linha do arquivo temporário
 while IFS= read -r FILE
 do
+
   if [ -d "$SRC$FILE" ] # verifica se é um pacote
   then
+
     if grep -q "public static void main(" "$SRC$FILE"/* 2>/dev/null # procura pelo método main
     then
       COM_PACOTES=1
       DIR_MAIN="$SRC$FILE"
       CLASS_MAIN=$(grep -rl "public static void main(" "$DIR_MAIN"/* 2>/dev/null | head -n 1) 
-      echo "$CLASS_MAIN" # apenas para fins de teste
+
+      for SRC_FILES in src/*
+      do
+        JAVA_COMPILE="$JAVA_COMPILE $SRC_FILES/*.java" # Compila código com todos os pacotes existentes na pasta src
+      done
+
+    JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # Atribuindo variável que de fato executa o projeto
     fi
+
   # else TODO quando não houver pacotes
   #   
   fi
@@ -48,3 +57,9 @@ done < tempfile.txt
 
 IFS=$OLDIFS
 rm tempfile.txt
+
+if [ "$COM_PACOTES" -eq 1 -o "$SEM_PACOTES" -eq 1 ]
+then
+  echo "teste"
+  # TODO implementar lógica de execução aqui
+fi
