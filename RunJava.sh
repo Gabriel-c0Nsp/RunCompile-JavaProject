44e491f21e4c15c16325a737be598b2730c47284
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-06 02:09:46 -0300

    Reescrita de código para compatibilidade com projetos que fazem uso de pacotes

--- RunJava.sh
+++ RunJava.sh
@@ -4,8 +4,8 @@
 #                                                                      #
 # RunJava.sh: Compila e executa projeto Java                           #
 #                                                                      # 
-# Auto: Gabriel Silva (gabrielsivaaires63@gmail.com)                   #    
-# Data criação: 02/09/2023                                             # 
+# Auto: Gabriel Silva (gabrielsivaaires64@gmail.com)                   #    
+# Data criação: 03/09/2023                                             # 
 #                                                                      #      
 # Descrição: Procura por todos os pacotes com arquivos .java e pelo    #
 # método main, compila e executa todo o projeto a partir do terminal   #
@@ -14,30 +14,37 @@
 #                                                                      #     
 ########################################################################
 
+clear 
 
-# Encontra todos os arquivos .java no diretório src e subdiretórios
-find src -name "*.java" > sources.txt # criação de arquivo temporário para segurar argumentos
-mkdir -p bin
-javac -d bin @sources.txt # compilação
+OLDIFS=$IFS # "backup" do IFS
+touch tempfile.txt # cria arquivo temporário que irá armazenar nome de cada arquivo da pasta src
+ls src > tempfile.txt # redireciona os arquivos listados para pasta temporária
 
-# Lista de classes com o método main
-classes_com_main=()
+SRC="src/"
+DIR_MAIN=""
+CLASS_MAIN="" 
+JAVA_COMPILE=""
+JAVA_EXECUTE="java -cp bin "
+COM_PACOTES=0
+SEM_PACOTES=0
 
-# Loop para encontrar classes com o método main
-while IFS= read -r file; do
-    if grep -qF "public static void main(String[]" "$file"; then
-        # Extrai o nome da classe do caminho do arquivo
-        class_name=$(basename "$file" .java)
-        classes_com_main+=("$class_name")
+# loop para ler cada linha do arquivo temporário
+while IFS= read -r FILE
+do
+  if [ -d "$SRC$FILE" ] # verifica se é um pacote
+  then
+    if grep -q "public static void main(" "$SRC$FILE"/* 2>/dev/null # procura pelo método main
+    then
+      COM_PACOTES=1
+      DIR_MAIN="$SRC$FILE"
+      CLASS_MAIN=$(grep -rl "public static void main(" "$DIR_MAIN"/* 2>/dev/null | head -n 1) 
+      echo "$CLASS_MAIN" # apenas para fins de teste
     fi
-done < sources.txt
+  # else TODO quando não houver pacotes
+  #   
+  fi
+      
+done < tempfile.txt
 
-
-# Executa as classes com o método main
-for class in "${classes_com_main[@]}"; do
-    java -cp bin "$class"
-done
-
-# Limpa o arquivo temporário
-rm sources.txt
-
+IFS=$OLDIFS
+rm tempfile.txt
