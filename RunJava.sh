9da13111487cf01c09dc8c0fe6f390c792d08dfb
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-07 13:45:00 -0300

    Conclusão do script (TODO: adicionar melhorias + documentação)

--- RunJava.sh
+++ RunJava.sh
@@ -4,7 +4,7 @@
 #                                                                      #
 # RunJava.sh: Compila e executa projeto Java                           #
 #                                                                      # 
-# Auto: Gabriel Silva (gabrielsivaaires64@gmail.com)                   #    
+# Autor: Gabriel Silva Aires (gabrielsilvaaires62@gmailcom)            #    
 # Data criação: 03/09/2023                                             # 
 #                                                                      #      
 # Descrição: Procura por todos os pacotes com arquivos .java e pelo    #
@@ -32,34 +32,44 @@ SEM_PACOTES=0
 while IFS= read -r FILE
 do
 
-  if [ -d "$SRC$FILE" ] # verifica se é um pacote
+  if [ -d "$SRC$FILE" ] # verifica se é um pacote (diretório)
   then
 
     if grep -q "public static void main(" "$SRC$FILE"/* 2>/dev/null # procura pelo método main
     then
       COM_PACOTES=1
       DIR_MAIN="$SRC$FILE"
-      CLASS_MAIN=$(grep -rl "public static void main(" "$DIR_MAIN"/* 2>/dev/null | head -n 1) 
+      CLASS_MAIN=$(grep -rl "public static void main(" "$DIR_MAIN"/* 2>/dev/null | head -n 1) # atribui variável que contém classe com método main
 
       for SRC_FILES in src/*
       do
         JAVA_COMPILE="$JAVA_COMPILE $SRC_FILES/*.java" # Compila código com todos os pacotes existentes na pasta src
       done
 
-    JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # Atribuindo variável que de fato executa o projeto
+      JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # Atribuindo variável que de fato executa o projeto
     fi
 
-  # else TODO quando não houver pacotes
-  #   
+  elif [ ! -d "$SRC$FILE" ] # verifica que não é um pacote (diretório)
+    JAVA_COMPILE="$JAVA_COMPILE $SRC$FILE" # Compila código com todos os arquivos.java existentes na pasta src
+  then 
+    SEM_PACOTES=1
+
+    if grep -q "public static void main(" "$SRC$FILE" 2>/dev/null # procura pelo método main
+    then
+      CLASS_MAIN="$SRC$FILE" # atribui variável que contém classe com método main
+      CLASS_MAIN=$(basename "$CLASS_MAIN" .java) # remove conteúdo desnecessário de variável CLASS_MAIN
+      JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # Atribuindo variável que de fato executa o projeto
+    fi
+
   fi
-      
+
 done < tempfile.txt
 
 IFS=$OLDIFS
-rm tempfile.txt
+rm tempfile.txt # remove arquivo temporário
 
-if [ "$COM_PACOTES" -eq 1 -o "$SEM_PACOTES" -eq 1 ]
+if [ "$COM_PACOTES" -eq 1 -o "$SEM_PACOTES" -eq 1 ] # Roda o projeto no terminal
 then
-  echo "teste"
-  # TODO implementar lógica de execução aqui
+  eval $JAVA_COMPILE
+  eval $JAVA_EXECUTE
 fi
