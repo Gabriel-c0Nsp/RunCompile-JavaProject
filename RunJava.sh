cfd0cf64bd63f654809dd4e3ce173f864e2247ee
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-07 22:39:25 -0300

    Tradução de comentários para inglês

--- RunJava.sh
+++ RunJava.sh
@@ -2,23 +2,23 @@
 
 ########################################################################
 #                                                                      #
-# RunJava.sh: Compila e executa projeto Java                           #
+# RunJava.sh: Compile and execute Java project                         #
 #                                                                      # 
-# Autor: Gabriel Silva Aires (gabrielsilvaaires62@gmailcom)            #    
-# Data criação: 03/09/2023                                             # 
+# Author: Gabriel Silva Aires (gabrielsilvaaires@gmail.com)            #    
+# Date: 09/03/2023                                                     # 
 #                                                                      #      
-# Descrição: Procura por todos os pacotes com arquivos .java e pelo    #
-# método main, compila e executa todo o projeto a partir do terminal   #
+# Description: Search for packages or files.java, compile and execute  #
+# Java code from the terminal.                                         #
 #                                                                      #        
-# Exemplo de uso: RunJava.sh | runjava (com alias)                     #
+# Example: RunJava.sh | runjava (com alias)                            #
 #                                                                      #     
 ########################################################################
 
 clear 
 
-OLDIFS=$IFS # "backup" do IFS
-touch tempfile.txt # cria arquivo temporário que irá armazenar nome de cada arquivo da pasta src
-ls src > tempfile.txt # redireciona os arquivos listados para pasta temporária
+OLDIFS=$IFS # "backup" IFS
+touch tempfile.txt # create temporary file and storage every file's name from src directory
+ls src > tempfile.txt # redirect the listed files to the temporary file
 
 SRC="src/"
 DIR_MAIN=""
@@ -28,37 +28,37 @@ JAVA_EXECUTE="java -cp bin"
 COM_PACOTES=0
 SEM_PACOTES=0
 
-# loop para ler cada linha do arquivo temporário
+# loop to read each line from the temporary file
 while IFS= read -r FILE
 do
 
-  if [ -d "$SRC$FILE" ] # verifica se é um pacote (diretório)
+  if [ -d "$SRC$FILE" ] # check if it's a package (directory)
   then
 
-    if grep -q "public static void main(" "$SRC$FILE"/* 2>/dev/null # procura pelo método main
+    if grep -q "public static void main(" "$SRC$FILE"/* 2>/dev/null # search for main method
     then
       COM_PACOTES=1
       DIR_MAIN="$SRC$FILE"
-      CLASS_MAIN=$(grep -rl "public static void main(" "$DIR_MAIN"/* 2>/dev/null | head -n 1) # atribui variável que contém classe com método main
+      CLASS_MAIN=$(grep -rl "public static void main(" "$DIR_MAIN"/* 2>/dev/null | head -n 1) # assign a variable that contains the class with the main method
 
       for SRC_FILES in src/*
       do
-        JAVA_COMPILE="$JAVA_COMPILE $SRC_FILES/*.java" # Compila código com todos os pacotes existentes na pasta src
+        JAVA_COMPILE="$JAVA_COMPILE $SRC_FILES/*.java" # compile code with all existing packages in the src folder
       done
 
-      JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # Atribuindo variável que de fato executa o projeto
+      JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # assign a variable that actually runs the project
     fi
 
-  elif [ ! -d "$SRC$FILE" ] # verifica que não é um pacote (diretório)
-    JAVA_COMPILE="$JAVA_COMPILE $SRC$FILE" # Compila código com todos os arquivos.java existentes na pasta src
+  elif [ ! -d "$SRC$FILE" ] # check that it's not a package (directory)
+    JAVA_COMPILE="$JAVA_COMPILE $SRC$FILE" # compile code with all .java files existing in the src folder
   then 
     SEM_PACOTES=1
 
-    if grep -q "public static void main(" "$SRC$FILE" 2>/dev/null # procura pelo método main
+    if grep -q "public static void main(" "$SRC$FILE" 2>/dev/null # search for main method
     then
-      CLASS_MAIN="$SRC$FILE" # atribui variável que contém classe com método main
-      CLASS_MAIN=$(basename "$CLASS_MAIN" .java) # remove conteúdo desnecessário de variável CLASS_MAIN
-      JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # Atribuindo variável que de fato executa o projeto
+      CLASS_MAIN="$SRC$FILE" # assign a variable that contains the class with the main method
+      CLASS_MAIN=$(basename "$CLASS_MAIN" .java) # remove unnecessary content from the CLASS_MAIN variable
+      JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # assign a variable that actually runs the project
     fi
 
   fi
@@ -66,9 +66,9 @@ do
 done < tempfile.txt
 
 IFS=$OLDIFS
-rm tempfile.txt # remove arquivo temporário
+rm tempfile.txt # remove temporary file
 
-if [ "$COM_PACOTES" -eq 1 -o "$SEM_PACOTES" -eq 1 ] # Roda o projeto no terminal
+if [ "$COM_PACOTES" -eq 1 -o "$SEM_PACOTES" -eq 1 ] # run project from the terminal
 then
   eval $JAVA_COMPILE
   eval $JAVA_EXECUTE
