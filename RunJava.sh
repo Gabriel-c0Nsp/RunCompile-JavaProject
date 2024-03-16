1e8fa14941bcf51fab3a7c3d565f6c7adc51b726
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-03-15 22:45:51 -0300

    Implementação de função para ir para o diretório raiz, e da função para achar o tipo de projeto

--- RunJava.sh
+++ RunJava.sh
@@ -1,4 +1,6 @@
-#!/bin/zsh
+#!/bin/bash
+
+# THIS PROJECT IS NOT USABLE YET. FOR STABLE VERSION, USE PREVOIUS VERSIONS
 
 ########################################################################
 #                                                                      #
@@ -14,74 +16,61 @@
 #                                                                      #     
 ########################################################################
 
-clear 
+clear
 
-CURRENT_DIR=$(pwd) # get the current directory
-KEYWORD="src" 
-ROOT_DIR="$CURRENT_DIR"
+type_of_project=""
+# options: "with_package", "no_package", "maven", "gradle"
 
-while [[ "$ROOT_DIR" == *"$KEYWORD"* ]] # while ROOT_DIR contains the keyword KEYWORD, remove the last directory
-do
-  ROOT_DIR=$(dirname "$ROOT_DIR")
-done
+go_to_root() {
+  local found=0
 
-cd $ROOT_DIR
-
-OLDIFS=$IFS # "backup" IFS
-touch tempfile.txt # create temporary file and storage every file's name from src directory
-ls src > tempfile.txt # redirect the listed files to the temporary file
-
-SRC="src/"
-DIR_MAIN=""
-CLASS_MAIN="" 
-JAVA_COMPILE="javac -d bin"
-JAVA_EXECUTE="java -cp bin"
-COM_PACOTES=0
-SEM_PACOTES=0
-
-# loop to read each line from the temporary file
-while IFS= read -r FILE
-do
-
-  if [ -d "$SRC$FILE" ] # check if it's a package (directory)
-  then
-
-    if grep -q "public static void main(" "$SRC$FILE"/* 2>/dev/null # search for main method
-    then
-      COM_PACOTES=1
-      DIR_MAIN="$SRC$FILE"
-      CLASS_MAIN=$(grep -rl "public static void main(" "$DIR_MAIN"/* 2>/dev/null | head -n 1) # assign a variable that contains the class with the main method
-
-      for SRC_FILES in src/*
-      do
-        JAVA_COMPILE="$JAVA_COMPILE $SRC_FILES/*.java" # compile code with all existing packages in the src folder
-      done
-
-      JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # assign a variable that actually runs the project
+  while [[ $found -ne 1 ]]; do
+    if [[ -d "src" ]]; then
+      echo "$(pwd)"
+      cd "src"
+      found=1
+    else 
+      if [[ "$(pwd)" != "$HOME" ]]; then
+        cd ..
+      else # then there is no src folder in the project
+        break
+      fi
     fi
+  done 
+}
 
-  elif [ ! -d "$SRC$FILE" ] # check that it's not a package (directory)
-    JAVA_COMPILE="$JAVA_COMPILE $SRC$FILE" # compile code with all .java files existing in the src folder
-  then 
-    SEM_PACOTES=1
+get_type_of_project() {
+  local found=0
+  local current_path=$(pwd)
 
-    if grep -q "public static void main(" "$SRC$FILE" 2>/dev/null # search for main method
-    then
-      CLASS_MAIN="$SRC$FILE" # assign a variable that contains the class with the main method
-      CLASS_MAIN=$(basename "$CLASS_MAIN" .java) # remove unnecessary content from the CLASS_MAIN variable
-      JAVA_EXECUTE="$JAVA_EXECUTE $CLASS_MAIN" # assign a variable that actually runs the project
+  while [[ $found -ne 1 ]]; do
+    if [[ -d "src" ]]; then
+      if [[ -e "build.gradle" ]]; then
+        type_of_project="gradle"
+      elif [[ -e "pom.xml" ]]; then
+        type_of_project="maven"
+      else
+        type_of_project="with_package"
+      fi
+      found=1
+    else 
+      if [[ "$(pwd)" != "$HOME" ]]; then
+        cd ..
+      else 
+        cd $current_path
+        if find . -maxdepth 1 -type f -name "*.java" | grep -q .; then # check if there is any .java file
+            type_of_project="no_package"
+        fi
+        break
+      fi
     fi
+  done 
 
+  # check if type_of_project is empty
+  if [[ -z $type_of_project ]]; then
+    echo "This project is not a Java project"
   fi
+  cd $current_path
+}
 
-done < tempfile.txt
-
-
-IFS=$OLDIFS
-rm tempfile.txt # remove temporary file
-
-if [ "$COM_PACOTES" -eq 1 -o "$SEM_PACOTES" -eq 1 ] # run project from the terminal
-then
-  eval $JAVA_COMPILE
-  eval $JAVA_EXECUTE
-fi
+get_type_of_project
---
+++ test/gradle_project/gradle_project/src/main/java/com/example/gradle_projecta/GradleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle_projecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleProjectApplication.class, args);
+	}
+
+}
